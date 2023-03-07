87a7e14f8eabd28974cfe567a04a9f4a1eb5c671
Artyom <cash.kin.art@gmail.com>
2023-03-07 10:10:38 +0200

    lesson 31

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,6 +1,28 @@
 export class Excel {
   constructor(selector, options) {
-    this.$el = document.querySelector('selector');
+    this.$el = document.querySelector('#app');
     this.components = options.components || [];
+  }
+  getRoot() {
+    const $root = document.createElement('div');
+    // console.log(this.components);
+
+    this.components.forEach((Component) => {
+      const component = new Component();
+      $root.insertAdjacentHTML('beforeend', component.toHTML());
+    });
+    // $root.textContent = 'test';
+    // $root.style.fontSize = '5rem';
+    return $root;
+  }
+
+  render() {
+    // console.log(this.$el);
+    // afterbegin, afterned, beforeend, beforebegin
+    // this.$el.insertAdjacentHTML('afterbegin', `<h1>Test</h1>`);
+    // this.$el.append(document.createElement('h1'));
+    // const node = document.createElement('h1');
+    // node.textContent = 'TEST';
+    this.$el.append(this.getRoot());
   }
 }
