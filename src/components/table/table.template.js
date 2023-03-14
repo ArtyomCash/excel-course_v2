45d66a21d900ef8af730a3a2be7ecf064289e440
Artyom <cash.kin.art@gmail.com>
2023-03-14 10:57:32 +0200

    lesson 38

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,11 +3,11 @@ const CODES = {
   Z: 90,
 };
 
-/* function createCell() {
+function toCell() {
   return `
-    <div class="cell" contenteditable>B2</div>
+    <div class="sell" contenteditable></div>
   `;
-}*/
+}
 
 function toColumn(col) {
   return `
@@ -17,10 +17,10 @@ function toColumn(col) {
     `;
 }
 
-function createRow(content) {
+function createRow(index, content) {
   return `
     <div class="row">
-      <div class="row-info"></div>
+      <div class="row-info">${index ? index : ''}</div>
       <div class="row-data">${content}</div>
     </div>
   `;
@@ -38,10 +38,14 @@ export function createTable(rowCount = 15) {
   const cols = new Array(colsCount).fill('').map(toChar).map(toColumn).join('');
 
   console.log('cols', cols);
-  rows.push(createRow(cols));
+  rows.push(createRow(null, cols));
 
   for (let i = 0; i < rowCount; i++) {
-    rows.push(createRow());
+    const cells = new Array(colsCount)
+        .fill('')
+        .map(toCell)
+        .join('');
+    rows.push(createRow(i + 1, cells));
   }
 
   return rows.join('');
