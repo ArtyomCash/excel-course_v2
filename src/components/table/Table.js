cf161811784697b50306b96479d6ad2fa280701a
Artyom <cash.kin.art@gmail.com>
2023-03-18 10:41:17 +0200

    lesson 43

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -7,11 +7,20 @@ export class Table extends ExcelComponent {
   constructor($root) {
     super($root, {
       // listeners: ['click', 'mousedown', 'mousemove', 'mouseup'],
+      listeners: ['mousedown'],
     });
   }
 
   toHTML() {
     return createTable(20);
+  }
+
+  onMousedown(event) {
+    // console.log('mousedown', event.target.getAttribute('data-resize'));
+    // console.log('mousedown', event.target.dataset);
+    if (event.target.dataset.resize) {
+      console.log('Start resizing', event.target.dataset.resize);
+    }
   }
 
   /* onClick() {
