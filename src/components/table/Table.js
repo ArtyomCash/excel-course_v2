54269496d4b9dea84e67be3a7ebf2cdc15b817dc
Artyom <cash.kin.art@gmail.com>
2023-03-22 18:42:15 +0200

    lesson 49

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -28,22 +28,29 @@ export class Table extends ExcelComponent {
       const $perent = $resizer.closest('[data-type="resizable"]');
       const coords = $perent.getCoords();
       const type = $resizer.data.resize;
+      const sideProp = type === 'col' ? 'bottom' : 'right';
+      let value;
+
+      $resizer.css({
+        opacity: 1,
+        // zIndex: 1000,
+        [sideProp]: '-5000px',
+      });
 
       console.log(type);
 
-      const cell = this.$root.findAll(`[data-col="${$perent.data.col}"]`);
+      // const cell = this.$root.findAll(`[data-col="${$perent.data.col}"]`);
 
       document.onmousemove = (e) => {
         if (type === 'col') {
           const delta = e.pageX - coords.right;
-          const value = coords.width + delta;
+          value = coords.width + delta;
+          $resizer.css({ right: -delta + 'px' });
           // $perent.$el.style.width = value + 'px';
-          $perent.css({width: value + 'px'});
-          cell.forEach((el) => (el.style.width = value + 'px'));
         } else {
           const delta = e.pageY - coords.bottom;
-          const value = coords.height + delta;
-          $perent.css({height: value + 'px'});
+          value = coords.height + delta;
+          $resizer.css({ bottom: -delta + 'px' });
           // $perent.$el.style.height = value + 'px';
         }
         // e.pageX текщее положение мыши, отсчитывается с левого края
@@ -58,6 +65,20 @@ export class Table extends ExcelComponent {
       // удаляю событие на мышки когда отпускаю кнопку мышки
       document.onmouseup = () => {
         document.onmousemove = null;
+        document.onmouseup = null;
+
+        if (type === 'col') {
+          $perent.css({ width: value + 'px' });
+          this.$root.findAll(`[data-col="${$perent.data.col}"]`).forEach((el) => (el.style.width = value + 'px'));
+        } else {
+          $perent.css({ height: value + 'px' });
+        }
+
+        $resizer.css({
+          opacity: 0,
+          bottom: 0,
+          right: 0,
+        });
       };
     }
   }
