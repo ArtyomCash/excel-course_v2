3dfba5d2b579e428fde6bf1565ee7fc80c1d5ea1
Artyom <cash.kin.art@gmail.com>
2023-03-28 12:09:09 +0300

    lesson 50

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,6 +1,7 @@
 import { ExcelComponent } from '@core/ExcelComponent';
 import { createTable } from '@/components/table/table.template';
-import { $ } from '@core/dom';
+import { resizeHandler } from '@/components/table/table.resize';
+import { shouldResize } from '@/components/table/table.functions';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
@@ -19,67 +20,9 @@ export class Table extends ExcelComponent {
   onMousedown(event) {
     // console.log('mousedown', event.target.getAttribute('data-resize'));
     // console.log('mousedown', event.target.dataset);
-    if (event.target.dataset.resize) {
+    if (shouldResize(event)) {
       // console.log('Start resizing', event.target.dataset.resize);
-      const $resizer = $(event.target);
-      // const $perent = $resizer.$el.parentNode;
-      // .closest() - получить ближайшего родителя по условию
-      // const $perent = $resizer.$el.closest('.column');
-      const $perent = $resizer.closest('[data-type="resizable"]');
-      const coords = $perent.getCoords();
-      const type = $resizer.data.resize;
-      const sideProp = type === 'col' ? 'bottom' : 'right';
-      let value;
-
-      $resizer.css({
-        opacity: 1,
-        // zIndex: 1000,
-        [sideProp]: '-5000px',
-      });
-
-      console.log(type);
-
-      // const cell = this.$root.findAll(`[data-col="${$perent.data.col}"]`);
-
-      document.onmousemove = (e) => {
-        if (type === 'col') {
-          const delta = e.pageX - coords.right;
-          value = coords.width + delta;
-          $resizer.css({ right: -delta + 'px' });
-          // $perent.$el.style.width = value + 'px';
-        } else {
-          const delta = e.pageY - coords.bottom;
-          value = coords.height + delta;
-          $resizer.css({ bottom: -delta + 'px' });
-          // $perent.$el.style.height = value + 'px';
-        }
-        // e.pageX текщее положение мыши, отсчитывается с левого края
-        // console.log('e.pageX>>>', e.pageX);
-        /* console.log('mousemove');
-        const delta = e.pageX - coords.right;
-        const value = coords.width + delta;
-        $perent.$el.style.width = value + 'px';
-        cell.forEach((el) => el.style.width = value + 'px');*/
-        // console.log('delta', delta);
-      };
-      // удаляю событие на мышки когда отпускаю кнопку мышки
-      document.onmouseup = () => {
-        document.onmousemove = null;
-        document.onmouseup = null;
-
-        if (type === 'col') {
-          $perent.css({ width: value + 'px' });
-          this.$root.findAll(`[data-col="${$perent.data.col}"]`).forEach((el) => (el.style.width = value + 'px'));
-        } else {
-          $perent.css({ height: value + 'px' });
-        }
-
-        $resizer.css({
-          opacity: 0,
-          bottom: 0,
-          right: 0,
-        });
-      };
+      resizeHandler(this.$root, event);
     }
   }
 
