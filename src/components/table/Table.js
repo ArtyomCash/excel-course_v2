0610d7c1d8567a8ab915f4158688a23935969df3
Artyom <cash.kin.art@gmail.com>
2023-04-04 22:25:12 +0300

    lesson 55

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -2,7 +2,7 @@ import { ExcelComponent } from '@core/ExcelComponent';
 import { $ } from '@core/dom';
 import { createTable } from '@/components/table/table.template';
 import { resizeHandler } from '@/components/table/table.resize';
-import {isCell, shouldResize} from '@/components/table/table.functions';
+import { isCell, shouldResize, matrix } from '@/components/table/table.functions';
 import { TableSelection } from '@/components/table/TableSelection';
 
 export class Table extends ExcelComponent {
@@ -38,23 +38,14 @@ export class Table extends ExcelComponent {
       resizeHandler(this.$root, event);
     } else if (isCell(event)) {
       const $target = $(event.target);
-      this.selection.select($target);
+      if (event.shiftKey) {
+        const $cells = matrix($target, this.selection.current)
+            .map((id) => this.$root.find(`[data-id="${id}"]`));
+        this.selection.selectGroup($cells);
+      } else {
+        this.selection.select($target);
+      }
     }
   }
-
-  /* onClick() {
-    console.log('click');
-  }
-
-  onMousedown(event) {
-    console.log('mousedown', event.target);
-  }
-
-  onMousemove() {
-    console.log('mousemove');
-  }
-
-  onMouseup() {
-    console.log('mouseup');
-  }*/
 }
+
